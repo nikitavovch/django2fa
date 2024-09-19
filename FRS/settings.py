96325495a3d5d881e15f3a45138af0aad1d01f4c
nikitavovch <kudiyakaa@gmail.com>
2024-09-19 09:09:43 +0500

    ttt

--- FRS/settings.py
+++ FRS/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 
 # Директории, где Django будет искать статические файлы
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
