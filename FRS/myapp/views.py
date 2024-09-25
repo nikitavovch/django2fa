b5912deb43b22ad7bfba0e504b19322ac9f53147
nikitavovch <kudiyakaa@gmail.com>
2024-09-26 03:10:52 +0500

    ttt

--- FRS/myapp/views.py
+++ FRS/myapp/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django_otp_webauthn.models import WebAuthnCredential
 
-
 def login_view(request):
-    next_url = request.GET.get('next', 'dashboard')  # По умолчанию перенаправляем на 'dashboard'
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -15,21 +12,16 @@ def login_view(request):
         if user is not None:
             login(request, user)
             if WebAuthnCredential.objects.filter(user=user).exists():
-                return redirect(next_url)
+                return redirect('dashboard')
             else:
-                return redirect('register_passkey')  # Если Passkey нет, регистрируем
+                return redirect('register_passkey')
         else:
             return render(request, 'templates/login.html', {'error': 'Неверный логин или пароль'})
-
     return render(request, 'templates/login.html')
-
 
 @login_required
 def passreg_view(request):
-    next_url = request.GET.get('next', 'dashboard')  # Для следующего перенаправления после регистрации Passkey
-    return render(request, 'templates/register_passkey.html', {'next': next_url})
-
-
+    return render(request, 'templates/register_passkey.html')
 @login_required
 def dashboard_view(request):
-    return render(request, 'templates/dashboard.html', {'username': request.user.username})
+    return render(request, 'templates/dashboard.html',  {'username': request.user.username})
